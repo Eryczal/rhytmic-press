d3206d99a8b412bb1b321d7c700f390fb19da2e3
Eryczal <79263592+Eryczal@users.noreply.github.com>
2024-09-22 09:43:18 +0200

    Clear drawing

--- assets/scripts/game/Game.js
+++ assets/scripts/game/Game.js
@@ -1,3 +1,4 @@
+import { CanvasManager } from "./managers/CanvasManager.js";
 import { EventManager } from "./managers/EventManager.js";
 import { ResizeManager } from "./managers/ResizeManager.js";
 import { SceneManager } from "./managers/SceneManager.js";
@@ -7,6 +8,7 @@ class Game {
         this.eventManager = new EventManager(this);
         this.sceneManager = new SceneManager(this);
         this.resizeManager = new ResizeManager(this);
+        this.canvasManager = new CanvasManager("canvas");
     }
 
     init() {
@@ -14,6 +16,8 @@ class Game {
     }
 
     draw() {
+        this.canvasManager.ctx.clearRect(0, 0, this.canvasManager.canvas.width, this.canvasManager.canvas.height);
+
         this.sceneManager.draw();
 
         window.requestAnimationFrame(() => this.draw());
